5322f910c139de6cf7ca9da37b1e5a8a3fe22d04
lburais <laurent@burais.fr>
2017-08-07 13:33:45 +0200

    correct folder + Domoticz

--- dietpi/osiris_setup.sh
+++ dietpi/osiris_setup.sh
@@ -2,10 +2,10 @@
 
 # -------- Own DietPi -------
 
-wget https://github.com/lburais/OsiriS/master/dietpi-config            -O /DietPi/dietpi/dietpi-config
-wget https://github.com/lburais/OsiriS/master/dietpi-set_hardware      -O /DietPi/dietpi/func/dietpi-set_hardware
-wget https://github.com/lburais/OsiriS/master/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a.dtbo
-wget https://github.com/lburais/OsiriS/master/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a-overlay.dtb
+wget https://github.com/lburais/OsiriS/master/dietpi/dietpi-config            -O /DietPi/dietpi/dietpi-config
+wget https://github.com/lburais/OsiriS/master/dietpi/dietpi-set_hardware      -O /DietPi/dietpi/func/dietpi-set_hardware
+wget https://github.com/lburais/OsiriS/master/dietpi/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a.dtbo
+wget https://github.com/lburais/OsiriS/master/dietpi/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a-overlay.dtb
 /DietPi/dietpi/func/dietpi-set_hardware lcdpanel waveshare35a
 
 # ------ Jeedom install -----
@@ -13,3 +13,8 @@ wget https://github.com/lburais/OsiriS/master/waveshare35a-overlay.dtb -O /boot/
 wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh
 chmod +x install.sh
 ./install.sh -w jeedom
+
+
+# ------ Domoticz install -----
+
+curl -L install.domoticz.com | sudo bash
