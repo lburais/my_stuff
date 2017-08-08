9544d96e334aeca30d4dc8ae78ca5b35a289b352
lburais <laurent@burais.fr>
2017-08-08 16:51:55 +0200

    DietPi simple setup

---
+++ DietPi/boot/Automation_Custom_Script.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# -------- Own DietPi -------
+
+#wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/dietpi-config            -O /DietPi/dietpi/dietpi-config
+#wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/dietpi-set_hardware      -O /DietPi/dietpi/func/dietpi-set_hardware
+#wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a.dtbo
+#wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a-overlay.dtb
+#/DietPi/dietpi/func/dietpi-set_hardware lcdpanel waveshare35a
+#con2fbmap 1 0
+
+# ------ Jeedom install -----
+
+wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh -O install_jeedom.sh
+chmod +x install_jeedom.sh
+./install_jeedom.sh -w /var/www/html/jeedom -z
--- dietpi/scp.sh
+++
@@ -1,6 +0,0 @@
-scp dietpi-set_hardware 					root@192.168.30.26:/DietPi/dietpi/func/dietpi-set_hardware
-scp dietpi-config       					root@192.168.30.26:/DietPi/dietpi/dietpi-config
-scp waveshare35a-overlay.dtb       			root@192.168.30.26:/boot/overlays/waveshare35a.dtbo
-scp waveshare35a-overlay.dtb       			root@192.168.30.26:/boot/overlays/waveshare35a-overlay.dtb
-scp dietpi-config       					root@192.168.30.26:/DietPi/dietpi/dietpi-config
-# scp root@192.168.30.26:/DietPi/dietpi.txt	dietpi.txt
