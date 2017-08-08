03053ac36023f8ba01d96dbff318f83682976e94
lburais <laurent@burais.fr>
2017-08-08 11:07:13 +0200

    remove domoticz

--- dietpi/osiris_setup.sh
+++ dietpi/osiris_setup.sh
@@ -7,14 +7,14 @@ wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/dietpi-set_h
 wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a.dtbo
 wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a-overlay.dtb
 /DietPi/dietpi/func/dietpi-set_hardware lcdpanel waveshare35a
+con2fbmap 1 0
 
 # ------ Jeedom install -----
 
-wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh
-chmod +x install.sh
-# ./install.sh -w jeedom
+wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh -O install_jeedom.sh
+chmod +x install_jeedom.sh
+./install_jeedom.sh -w /var/www/html/jeedom -z
 
+# ---------- reboot ---------
 
-# ------ Domoticz install -----
-
-# curl -L install.domoticz.com | sudo bash
+reboot
