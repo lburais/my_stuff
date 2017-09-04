1a249a3d70a4a30c2f42ea455ea9e7373960f351
lburais <laurent@burais.fr>
2017-09-04 18:32:40 +0200

    Spet 4

--- DietPi/boot/rpi-my-install.sh
+++ DietPi/boot/rpi-my-install.sh
@@ -13,4 +13,20 @@
 
 wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh -O install_jeedom.sh
 chmod +x install_jeedom.sh
-./install_jeedom.sh -w /var/www/html/jeedom -z
+./install_jeedom.sh
+
+# ------ Zulu (Java) install -----
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+echo 'deb http://repos.azulsystems.com/debian stable main' | tee  /etc/apt/sources.list.d/zulu.list
+apt-get update
+apt-get install zulu-embedded-8
+
+# ------ openHAB2 install -----
+
+wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | apt-key add -
+apt-get install apt-transport-https
+echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | tee /etc/apt/sources.list.d/openhab2.list
+apt-get update
+apt-get install openhab2
+apt-get install openhab2-addons
--- DietPi/osiris_setup.sh
+++ DietPi/osiris_setup.sh
@@ -2,19 +2,14 @@
 
 # -------- Own DietPi -------
 
-wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/dietpi-config            -O /DietPi/dietpi/dietpi-config
-wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/dietpi-set_hardware      -O /DietPi/dietpi/func/dietpi-set_hardware
-wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a.dtbo
-wget https://raw.githubusercontent.com/lburais/OsiriS/master/dietpi/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a-overlay.dtb
+wget https://github.com/lburais/OsiriS/master/dietpi-config            -O /DietPi/dietpi/dietpi-config
+wget https://github.com/lburais/OsiriS/master/dietpi-set_hardware      -O /DietPi/dietpi/func/dietpi-set_hardware
+wget https://github.com/lburais/OsiriS/master/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a.dtbo
+wget https://github.com/lburais/OsiriS/master/waveshare35a-overlay.dtb -O /boot/overlays/waveshare35a-overlay.dtb
 /DietPi/dietpi/func/dietpi-set_hardware lcdpanel waveshare35a
-con2fbmap 1 0
 
 # ------ Jeedom install -----
 
-wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh -O install_jeedom.sh
-chmod +x install_jeedom.sh
-./install_jeedom.sh -w /var/www/html/jeedom -z
-
-# ---------- reboot ---------
-
-reboot
+wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh
+chmod +x install.sh
+./install.sh -w jeedom
